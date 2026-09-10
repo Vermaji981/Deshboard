3e0457f3800c4a2e949e9a9341157791a4c73173
Vermaji981 <hv0563163@gmai.com>
2026-09-10 10:26:02 +0530

    Add error handler to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,23 @@
-const app = require("../backend/index.js");
+const express = require("express");
+const cors = require("cors");
+
+let app;
+
+try {
+    app = require("../backend/index.js");
+} catch (err) {
+    console.error("Failed to load backend server:", err);
+    const errApp = express();
+    errApp.use(cors());
+    errApp.use(express.json());
+    errApp.all("*", (req, res) => {
+        res.status(500).json({
+            error: "Backend Server Initialization Error",
+            message: err.message,
+            stack: err.stack
+        });
+    });
+    app = errApp;
+}
 
 module.exports = app;
